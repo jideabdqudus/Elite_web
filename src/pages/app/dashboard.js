dd3bbdbae3174a50f3395a98f4690bf37205d248
jideabdqudus <jideabdqudus@gmail.com>
2020-08-21 17:49:34 +0100

    Added Commit

---
+++ src/pages/app/dashboard.js
@@ -0,0 +1,17 @@
+import React from "react"
+import Sidebar from "../../components/Sidebar"
+import Dashboard from "../../pages2/dashboard/Dashboard"
+import Header from "../../components/Header"
+import s from "../../components/Layout/Layout.module.scss"
+import Hammer from "rc-hammerjs"
+import { TransitionGroup, CSSTransition } from "react-transition-group"
+
+const Dash = () => {
+  return (
+    <div>
+      <h1>Dashboard ge</h1>
+    </div> 
+  )
+}
+
+export default Dash
